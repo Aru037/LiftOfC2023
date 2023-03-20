44a0ce6dd77b6294670465478d014b997c493377
Aru037 <127929314+Aru037@users.noreply.github.com>
2023-03-20 21:01:17 +0530

    Add files via upload

---
+++ aru_sum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int SumDigits(int n);
+int main()
+{
+    int number;
+    printf("please enter your number: ");
+    scanf("%d",&number);
+    printf("the sum of digits of given number is %d",SumDigits(number));
+    return 0;
+}
+
+int SumDigits(int n){
+    int rem=0,sum=0;
+    for(int i=0;n>0;i++)
+    {
+        rem = n%10;
+        sum += rem;
+        n = n/10;
+    }
+    return sum;
+}
